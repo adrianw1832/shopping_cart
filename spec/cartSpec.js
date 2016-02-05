f07263f3a10d7b95806cf33d6468284b2598e79f
Adrian Wong <adrianwong1832@gmail.com>
2016-02-05 00:59:55 +0000

    Add the ability to add items to the cart

--- spec/cartSpec.js
+++ spec/cartSpec.js
@@ -9,5 +9,10 @@ describe('Cart', function() {
     it('starts off empty', function() {
       expect(cart.contents).toEqual({});
     });
+
+    it('user can add an item to it', function() {
+      cart.addItem('Suede Shoes, Blue', 1);
+      expect(cart.contents).toEqual({'Suede Shoes, Blue': 1});
+    });
   });
 });
--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,8 @@
 function Cart() {
   this.contents = {};
 }
+
+Cart.prototype.addItem = function(item, amount) {
+  typeof this.contents[item] === 'undefined' ? this.contents[item] = amount : this.contents[item] += amount;
+};
+
