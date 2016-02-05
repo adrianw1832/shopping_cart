d25ce5e7b4d05e9aafc9eac91320494710b63aa9
Adrian Wong <adrianwong1832@gmail.com>
2016-02-05 01:46:42 +0000

    Add more tests for the add item function for rigidity

--- spec/cartSpec.js
+++ spec/cartSpec.js
@@ -14,5 +14,17 @@ describe('Cart', function() {
       cart.addItem('Suede Shoes, Blue', 1);
       expect(cart.contents).toEqual({'Suede Shoes, Blue': 1});
     });
+
+    it('user can add two of the same item to it', function() {
+      cart.addItem('Suede Shoes, Blue', 1);
+      cart.addItem('Suede Shoes, Blue', 1);
+      expect(cart.contents).toEqual({'Suede Shoes, Blue': 2});
+    });
+
+    it('user can add two different items to it', function() {
+      cart.addItem('Suede Shoes, Blue', 1);
+      cart.addItem('Flip Flops, Red', 2);
+      expect(cart.contents).toEqual({'Suede Shoes, Blue': 1, 'Flip Flops, Red': 2});
+    });
   });
 });
