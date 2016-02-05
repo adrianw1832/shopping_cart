d7e5bd51e6db44840b6375dc725a5a1f4b6d3d37
Adrian Wong <adrianwong1832@gmail.com>
2016-02-05 05:48:46 +0000

    Add a lot of jQuery to connect the logic to webpage

--- src/application.js
+++ src/application.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
   var cart = new Cart();
+  var nameArray = Object.keys(cart.catalogue.items);
 
-  $('#total').html(cart.total);
+  var update = function() {
+    $('#total').html(cart.total);
+    for (var i = 0; i < nameArray.length; i++) {
+      var name = nameArray[i];
+      $('.item' + i).html(cart.catalogue.items[name].stock);
+    }
+  };
+
+  var buildButtons = function() {
+    for (i = 0; i < nameArray.length; i++) {
+      var name = nameArray[i];
+      var add = $('<input type="submit">').val('+').data('name', name)
+      .on('click', $.proxy(addItem, this));
+      var remove = $('<input type="submit">').val('-').data('name', name)
+      .on('click', $.proxy(removeItem, this));
+      var stock = $('<p>').addClass('item' + i);
+      var li = $('<li>').append(remove).append(name).append(add).append(stock);
+      $('.buttons').append(li);
+    }
+  };
+
+  var addItem = function(button) {
+    var value = $(button.currentTarget).data().name;
+    cart.addItem(value, 1);
+    update();
+  };
+
+  var removeItem = function(button) {
+    var value = $(button.currentTarget).data().name;
+    cart.removeItem(value, 1);
+    update();
+  };
+
+  $('#enter').click(function() {
+    var code = $('.form').val();
+    cart.applyVoucher(code);
+    update();
+  });
+
+  buildButtons();
+  update();
 });
